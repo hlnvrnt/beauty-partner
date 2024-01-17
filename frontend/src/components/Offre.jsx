9e6b7c404894cd7efaaa7c4102c5bf54e272eac6
Vinc441 <vincentlouvart82@gmail.com>
2024-01-17 16:43:47 +0100

    commit before pull dev

---
+++ frontend/src/components/Offre.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Offre() {
+  return (
+    <div className="container-page-offre">
+      <div className="nouveaute-offre">
+        <h1>NOUVEAUTES</h1>
+        <div className="offre-nouveaute1">
+          <div className="rond1" />
+        
+
+        <div className="rond2" />
+        </div>
+      </div>
+      <div className="container-offre">
+        <h1>NOS OFFRES</h1>
+        <div className="offre">
+          <div className="offre-1">
+            <div className="rond1" />
+            <h2>10 000pts</h2>
+            <h3>COFFRET CADEAU & PROMOTION</h3>
+            <p>Decouvrez-ici</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Offre from '../components/Offre';
 
 
 const Home = () => {
     return (
-        <div>
-            HOME 
-        </div>
+        <><div>
+        HOME 
+    </div>
+    <div><Offre /></div></>
+        
     );
 };
 
