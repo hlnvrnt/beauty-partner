e3730f711488f86e7af197248579f8d8f13cb6fe
Alexandra <alexandradepourtoux93@gmai.com>
2024-01-17 17:05:42 +0100

    page innovation, css scrollbar, css navbar

--- frontend/src/pages/InnovationAI.jsx
+++ frontend/src/pages/InnovationAI.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-
-function InnovationAI () {
-    return (
-        <div>
-            Borne
-        </div>
-    );
-};
+function InnovationAI() {
+  return (
+    <div className="innovation-container">
+      <div className="img-container">
+        <img src="images/innovation.jpg" alt="innovation" />
+      </div>
+      {/* <div className="header-wrapper">
+        
+      </div> */}
+      <div className="content-wrapper">
+        <h1>Innovation AI</h1>
+        <hr></hr>
+        <p>
+          Découvrez INNOVATION AI, l'avenir de la personnalisation capillaire
+          avec notre borne d'Intelligence Artificielle intégrée au salon de
+          coiffure. Offrant une expérience sur mesure, cette technologie
+          innovante permet à nos clients de choisir le produit parfait en
+          fonction de leurs critères uniques. Que ce soit le type de cheveux, la
+          texture, les besoins spécifiques ou les préférences , notre borne AI
+          analyse ces informations pour recommander les produits les mieux
+          adaptés à votre clientèle. Offrez une expérience de soin capillaire
+          personnalisée et immersive, redéfinissant ainsi les normes de
+          l'excellence en matière de coiffure. Une offre réservée au membres
+          Elite
+          <img src="images/logo-beauty-partner.jpg" alt="beautypartner" />
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default InnovationAI;
