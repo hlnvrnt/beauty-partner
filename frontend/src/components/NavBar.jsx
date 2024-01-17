3bec1181f0f02e7beed5da81fbe6fbc86195e087
hlnvrnt <helene.vernet@live.fr>
2024-01-17 15:28:06 +0100

    dossier public frontend

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 // import { useUser } from "../context/UserContext";
-
-const NavBar = () => {
-
-    // const { userInfos } = useUser();
-    return (
-        <div className="navbar">
-            NAVBAR
-        </div>
-    );
+function NavBar(){
+ // const { userInfos } = useUser();
+  return (
+    <div className="navbar">
+      <div className="marque">
+        <img src="/assets/images/logo.jpg" alt="logo"/>
+        <h1>BEAUTY PARTNER</h1>
+      </div>
+      <div className="connexion">
+        <NavLink to="/login">
+          <p>CONNEXION</p>
+        </NavLink>
+        <NavLink to="/register">
+          <p>INSCRIPTION</p>
+        </NavLink>
+      </div>
+      <NavLink to="/news">
+        <p>NOUVEAUTES</p>
+      </NavLink>
+      <NavLink to="/offres">
+        <p>NOS OFFRES</p>
+      </NavLink>
+      <NavLink to="/engagements">
+        <p>NOS ENGAGEMENTS</p>
+      </NavLink>
+      <NavLink to="/partenaires">
+        <p>NOS PARTENAIRES</p>
+      </NavLink>
+      <NavLink to="/innovation">
+        <p>INNOVATION AI</p>
+      </NavLink>
+    </div>
+  );
 };
 
 export default NavBar;
