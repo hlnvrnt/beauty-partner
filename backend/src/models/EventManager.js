c0e03a429403b836e8d1658671ca0e7900bd3fcc
hlnvrnt <helene.vernet@live.fr>
2024-01-18 11:27:13 +0100

    backend auth + page register

--- backend/src/models/EventManager.js
+++ backend/src/models/EventManager.js
@@ -4,6 +4,14 @@ class EventManager extends AbstractManager {
   constructor() {
     super({ table: "Event" });
   }
+
+  async readEventBySalonId(id) {
+    const [result] = await this.database.query(
+      `select salon.id, event_id, salon.name, salon.point, event.name, event.surname, event.value from ${this.table} JOIN salon ON salon_id=salon.id where event_id = ?`,
+      [id]
+    );
+    return result;
+  }
 }
 
 module.exports = EventManager;
